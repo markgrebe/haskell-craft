7e2f2e70504ddbabc3cd1499cbdbbbf7179ec780
Mark Grebe <mark.grebe@ku.edu>
2015-02-16 14:31:53 -0600

    Refactored gps example

--- examples/gps/Main.hs
+++ examples/gps/Main.hs
@@ -11,29 +11,27 @@ main = do
     -- Open a connection to the Minecraft game
     ch <- openCraft "192.168.200.107" "4711"
     -- Get the initial player position
-    oldPos <- send ch $ do
+    send ch $ do
         pos <- playerGetTile ()
-        return pos
-    loop ch oldPos
+        loop pos
 
-loop :: Handle -> (Int, Int, Int) -> IO ()
-loop ch oldPos = do
+loop :: (Int, Int, Int) -> Craft ()
+loop oldPos = do
     -- Get players new position
-    pos <- send ch $ playerGetTile()
+    pos <- playerGetTile()
     -- If it has changed, send a chat message with the position and the
     -- type of block they are standing on.
     if pos /= oldPos then do
         let x = getx pos
             y = gety pos
             z = getz pos
-        send ch $ do
-            block <- worldGetBlock(x, y-1, z)
-            chatPost(show x ++ " " ++ show y ++ " " ++
-                     show z ++ " "++ show block)
-            return ()
+        block <- worldGetBlock(x, y-1, z)
+        chatPost(show x ++ " " ++ show y ++ " " ++
+                 show z ++ " "++ show block)
+        return ()
     else
         return()
-    loop ch (if pos /= oldPos then pos else oldPos)
+    loop (if pos /= oldPos then pos else oldPos)
   where
     getx (x,y,z) = x
     gety (x,y,z) = y
